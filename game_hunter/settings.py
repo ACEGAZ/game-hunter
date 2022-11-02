14caafacc14e5b73a07e7117ae283d843b69c390
ACEGAZ <garylowles@gmail.com>
2022-11-02 19:29:32 +0000

    add crispy forms and styled log in page

--- game_hunter/settings.py
+++ game_hunter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
     'home',
 ]
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'game_hunter.urls'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
