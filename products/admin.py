21146e2d09e4b97e74a19469a8ccc3ef4a5e7f60
ACEGAZ <garylowles@gmail.com>
2022-11-04 11:20:42 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Categories, Products
 # Register your models here.
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'platform',
+        'price',
+        'image',
+    )
+
+
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Products, ProductsAdmin)
--- products/models.py
+++ products/models.py
@@ -2,19 +2,41 @@ from django.db import models
 
 # Create your models here.
 
+PS4 = 'PS4'
+PS5 = 'PS5'
+XBOX_360 = 'XBOX 360'
+XBOX_SERIES_X = 'XBOX SERIES X'
+SWITCH = 'SWITCH'
+
 
 class Categories(models.Model):
     name = models.CharField(max_length=254)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 
 class Products(models.Model):
+    PLATFORM_CHOICES = (
+                       (PS4, "PS4"),
+                       (PS5, "PS5"),
+                       (XBOX_360, "XBOX 360"),
+                       (XBOX_SERIES_X, "XBOX SERIES X"),
+                       (SWITCH, "SWITCH"),
+                       )
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
     category = models.ForeignKey('Categories', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    platform = models.CharField(max_length=254, null=True,
+                                choices=PLATFORM_CHOICES)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
