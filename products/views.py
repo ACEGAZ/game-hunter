c2ef23e647ccccdd3a604d1771945c62de104b56
ACEGAZ <garylowles@gmail.com>
2022-11-08 12:28:54 +0000

    add functionalitiy to games links

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Products
+from .models import Products, Categories
 
 # Create your views here.
 
@@ -9,9 +9,23 @@ from .models import Products
 def all_products(request):
     """ a view to show all products, including sorting and search queries"""
     products = Products.objects.all()
+
     query = None
+    categories = None
+    platforms = None
 
     if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Categories.objects.filter(name__in=categories)
+
+        if 'platform' in request.GET:
+            platforms = request.GET['platform'].split(',')
+            products = products.filter(platform__in=platforms)
+            platforms = Products.objects.filter(name__in=platforms)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +38,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'platform': platforms,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
