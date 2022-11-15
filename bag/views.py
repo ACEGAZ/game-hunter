6f8194c81e761b588e63688c83310c79bbab9c70
ACEGAZ <garylowles@gmail.com>
2022-11-15 14:30:49 +0000

    add success toast html and functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from products.models import Products
+
 # Create your views here.
 
 
@@ -11,14 +14,18 @@ def bag_view(request):
 def add_to_bag(request, item_id):
     """add item quantity to bag"""
 
+    product = Products.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'added {product.name} to your loot')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'added {product.name} to your loot')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
