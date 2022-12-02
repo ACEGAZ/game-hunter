f25b2cf22b169b913d99513ce027f225a7420c21
ACEGAZ <garylowles@gmail.com>
2022-12-02 12:15:22 +0000

    changed debug to development

--- game_hunter/settings.py
+++ game_hunter/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get('DEVELOPMENT', False)
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'game-hunter-website.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-gamehunter-pkgxu861c7s.ws-eu77.gitpod.io"]
