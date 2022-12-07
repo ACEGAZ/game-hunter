f54ff2f76f96aac6cb69bf79e2b53a872c527797
ACEGAZ <garylowles@gmail.com>
2022-12-07 22:33:01 +0000

    add functionality to edit reviews

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('<int:review_id>/', views.index, name='home'),
     path('add_review/', views.add_review, name='add_review'),
+    path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'),
     path('add_review_success/', views.add_review_success,
          name='add_review_success'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,12 +8,13 @@ from .forms import ReviewsForm
 # Create your views here.
 
 
-def index(request):
+def index(request, review_id):
     """ a view to return the index page"""
-
+    review = get_object_or_404(Reviews, pk=review_id)
     last_eight_reviews = Reviews.objects.all().order_by('-id')[:8][::-1]
     context = {
         'last_eight_reviews': last_eight_reviews,
+        'review': review
     }
     return render(request, 'home/index.html', context)
 
@@ -45,6 +46,35 @@ def add_review(request):
     }
 
     return render(request, 'home/add_reviews.html', context)
+
+
+@login_required
+def edit_review(request, review_id):
+    """for superusers to edit products in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    review = get_object_or_404(Reviews, pk=review_id)
+    if request.method == 'POST':
+        form = ReviewsForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully edited your review!')
+            return redirect(reverse('home', args=[review.id]))
+        else:
+            messages.error(request, 'Failed to edit your review. Please ensure the form is valid.')
+    else:
+        form = ReviewsForm(instance=review)
+        messages.info(request, f'You are editing your review titled {review.title}')
+
+    template = 'home/edit_review.html'
+    context = {
+        'form': form,
+        'review': review
+    }
+
+    return render(request, template, context)
 
 
 def add_review_success(request):
