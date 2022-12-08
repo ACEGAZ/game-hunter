3b501a40ffe2e5ac95df64c22d300274134b4afe
ACEGAZ <garylowles@gmail.com>
2022-12-08 18:26:27 +0000

    add email verification settings

--- game_hunter/settings.py
+++ game_hunter/settings.py
@@ -31,7 +31,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 # DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'game-hunter-website.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-gamehunter-pkgxu861c7s.ws-eu77.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = ["https://8000-acegaz-gamehunter-pkgxu861c7s.ws-eu78.gitpod.io"]
 
 
 # Application definition
@@ -213,3 +213,16 @@ if 'USE_AWS' in os.environ:
 # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+# Email config
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
