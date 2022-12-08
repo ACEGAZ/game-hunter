b03b15645fc7d402a4cd6fdb4911101754d5da0c
ACEGAZ <garylowles@gmail.com>
2022-12-08 10:45:32 +0000

    add delete review functionality

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,15 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('<int:review_id>/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('add_review/', views.add_review, name='add_review'),
-    path('edit_review/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('edit_review/<int:review_id>/', views.edit_review,
+         name='edit_review'),
+    path('delete_review_confirmation/<int:review_id>/',
+         views.delete_review_confirmation,
+         name='delete_review_confirmation'),
+    path('delete_review/<int:review_id>/', views.delete_review,
+         name='delete_review'),
     path('add_review_success/', views.add_review_success,
          name='add_review_success'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,9 +8,9 @@ from .forms import ReviewsForm
 # Create your views here.
 
 
-def index(request, review_id):
+def index(request):
     """ a view to return the index page"""
-    review = get_object_or_404(Reviews, pk=review_id)
+    review = Reviews.objects.all()
     last_eight_reviews = Reviews.objects.all().order_by('-id')[:8][::-1]
     context = {
         'last_eight_reviews': last_eight_reviews,
@@ -51,8 +51,8 @@ def add_review(request):
 @login_required
 def edit_review(request, review_id):
     """for superusers to edit products in the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, you need an account to edit a review.')
         return redirect(reverse('home'))
 
     review = get_object_or_404(Reviews, pk=review_id)
@@ -61,7 +61,7 @@ def edit_review(request, review_id):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully edited your review!')
-            return redirect(reverse('home', args=[review.id]))
+            return redirect(reverse('home'))
         else:
             messages.error(request, 'Failed to edit your review. Please ensure the form is valid.')
     else:
@@ -77,7 +77,34 @@ def edit_review(request, review_id):
     return render(request, template, context)
 
 
+@login_required
+def delete_review(request, review_id):
+    """ Delete a product from the store """
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, you need an account to delete a review.')
+        return redirect(reverse('home'))
+
+    review = get_object_or_404(Reviews, pk=review_id)
+    review.delete()
+    messages.success(request, 'Review deleted!')
+    return redirect(reverse('home'))
+
+
 def add_review_success(request):
     """ a view to return the add_review_success.html page"""
 
     return render(request, 'home/add_review_success.html')
+
+
+@login_required
+def delete_review_confirmation(request, review_id):
+    """ Render a view to confirm deletion of a product """
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, you need an account to delete a review.')
+        return redirect(reverse('home'))
+    review = get_object_or_404(Reviews, pk=review_id)
+    template = 'home/delete_review_confirmation.html'
+    context = {
+        'review': review
+    }
+    return render(request, template, context)
