343f52a2c66358d2c34563dfabaf3a1c4ff0902f
ACEGAZ <garylowles@gmail.com>
2022-12-09 14:20:00 +0000

    fixed error on edit product.html line 23

--- game_hunter/settings.py
+++ game_hunter/settings.py
@@ -217,7 +217,7 @@ if 'USE_AWS' in os.environ:
 # Email config
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'gamehunter@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
